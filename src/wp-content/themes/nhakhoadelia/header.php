976f4c2dcdd0026f006d56c23bd14a12fbef33da
Dung Vo The <dungvt@concrete-corp.com>
2023-10-13 18:22:17 +0700

    add tag google

--- src/wp-content/themes/nhakhoadelia/header.php
+++ src/wp-content/themes/nhakhoadelia/header.php
@@ -52,6 +52,15 @@
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-N479GN9');</script>
     <!-- End Google Tag Manager -->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11302488857"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'AW-11302488857');
+    </script>
   </head>
   <body <?php body_class(); ?>>
     <!-- Google Tag Manager (noscript) -->
